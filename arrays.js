e1693fc8c72bbe94b7afca5fc0874ea7b48628ef
Richard Mable <richardbmable@gmail.com>
2016-03-23 22:41:25 -0400

    completed through duplicates in arrays.js

--- arrays.js
+++ arrays.js
@@ -28,44 +28,107 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-    
-
+    // loop through all elements in the array
+    for (i = 0; i < arr.length; i++){
+      // if the array equals the item provided
+      if (arr[i] === item){
+        // remove it using .splice
+        arr.splice(i, 1);
+        // then add one to i as the array is now shorter
+        // otherwise the loop will skip over the next element
+        i = i - 1
+      }
+    }
+    // return the modified array, as .slice returns an array of the deleted elements
+    return arr
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    // not sure how this is different from .remove, but same idea
+    for (i = 0; i < arr.length; i++){
+      // if the array equals the item provided
+      if (arr[i] === item){
+        // remove it using .splice
+        arr.splice(i, 1);
+        // then add one to i as the array is now shorter
+        // otherwise the loop will skip over the next element
+        i = i - 1
+      }
+    }
+    // return the modified array, as .slice returns an array of the deleted elements
+    return arr
   },
 
   append : function(arr, item) {
-
+    // use .push to push the item onto the end of the array
+    // and return the newly modified array
+    arr.push(item)
+    return arr
   },
 
   truncate : function(arr) {
-
+    // use .pop to remove the last item in the array
+    arr.pop()
+    // return the modified array
+    return arr
   },
 
   prepend : function(arr, item) {
-
+    // reverse the array
+    arr.reverse();
+    // push the item onto the end of the array
+    arr.push(item);
+    // reverse the array to the original state with
+    // the new element prepended to the begining of the array
+    arr.reverse();
+    // return the array
+    return arr
   },
 
   curtail : function(arr) {
-
+    // reverse the array
+    arr.reverse();
+    // use .pop to remove the last element
+    arr.pop();
+    // reverse the array back to the original state
+    arr.reverse();
+    // return the array
+    return arr
   },
 
   concat : function(arr1, arr2) {
-
+    // join the two arrays using .concat and set to result
+    result = arr1.concat(arr2);
+    // return the result as expected in the test
+    return result
   },
 
   insert : function(arr, item, index) {
-
+    // use .splice to insert item by setting the howmany param to be 0
+    arr.splice(index, 0, item);
+    // return the modified array
+    return arr
   },
 
   count : function(arr, item) {
-
+    // set the counter to zero
+    result = 0;
+    // rotate through all elements in the array
+    for (i = 0; i < arr.length; i++){
+      // check to see if the current index matches the provided search element
+      if (arr[i] === item){
+        // if it does, increment the result by one
+        result++;
+      }
+    }
+    // return the total number of elements found
+    return result
   },
 
   duplicates : function(arr) {
-
+    for ( i = 0; i < arr.length; i++){
+      if 
+    }
   },
 
   square : function(arr) {
